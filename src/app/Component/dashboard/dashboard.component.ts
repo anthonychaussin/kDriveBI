5d901efd25dfbfd5401f2562c5459a5aeb542e9a
anthonychaussin <chaussin.anthony@gmail.com>
2024-07-14 20:08:03 +0200

    fix pdf print

--- src/app/Component/dashboard/dashboard.component.ts
+++ src/app/Component/dashboard/dashboard.component.ts
@@ -1,53 +1,59 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
 import html2canvas from 'html2canvas';
-import { jsPDF } from "jspdf";
+import {jsPDF} from 'jspdf';
 import {Activity} from '../../Model/activity';
 import {DataService} from '../../Service/data.service';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
-})
+             selector: 'app-dashboard',
+             templateUrl: './dashboard.component.html',
+             styleUrl: './dashboard.component.scss'
+           })
+
 export class DashboardComponent {
   activities: Activity[] = [];
   @ViewChild('contentToConvert') contentToConvert: ElementRef | undefined;
-  private isDarkMode: boolean = true;
-  private currentTheme: 'light-theme' | 'dark-theme' = 'dark-theme';
 
   constructor(protected dataService: DataService) {
     dataService.activitiesObservable.subscribe(activities => {
       // @ts-ignore
-      if(activities === null){
+      if (activities === null) {
         return;
       }
-      if(activities && activities.length > 0) {
+      if (activities && activities.length > 0) {
         this.activities = activities;
       }
     });
   }
 
-  public convetToPDF()
-  {
-    this.changeTheme();
-    html2canvas(this.contentToConvert!.nativeElement).then(canvas => {
-      // Few necessary setting options
-      const imgWidth = 200;
-      const imgHeight = canvas.height * imgWidth / canvas.width;
+  public convetToPDF() {
+    const htmlWidth = this.contentToConvert?.nativeElement.offsetWidth;
+    const htmlHeight = this.contentToConvert?.nativeElement.offsetHeight;
 
-      const contentDataURL = canvas.toDataURL('image/png')
-      let pdf = new jsPDF('p', 'mm', 'a4', true); // A4 size page of PDF
-      pdf.addImage(contentDataURL, 'PNG', 5, 5, imgWidth, imgHeight)
-      pdf.save('kDrive_ActivityReport.pdf'); // Generated PDF
-      this.changeTheme();
+    const topLeftMargin = 15;
+
+    let pdfWidth = (htmlWidth + (topLeftMargin * 2));
+    let pdfHeight = ((pdfWidth * 1.5) + (topLeftMargin * 2));
+
+    const canvasImageWidth = htmlWidth;
+    const canvasImageHeight = htmlHeight;
+
+    const totalPDFPages = Math.ceil(htmlHeight / pdfHeight) - 1;
+    const data = this.contentToConvert?.nativeElement;
+    html2canvas(data, {ignoreElements: element => element.classList.contains('dontPrint'), logging: false, scale: 2.5}).then(canvas => {
+
+      canvas.getContext('2d');
+
+      const imgData = canvas.toDataURL('image/jpeg');
+      let pdf = new jsPDF('p',  'pt', [pdfWidth, pdfHeight], true);
+      pdf.addImage(imgData, 'jpeg', topLeftMargin, topLeftMargin, canvasImageWidth, canvasImageHeight);
+
+      for (let i = 1; i <= totalPDFPages; i++) {
+        pdf.addPage([pdfWidth, pdfHeight], 'p');
+        pdf.addImage(imgData, 'jpeg', topLeftMargin, -(pdfHeight * i) + (topLeftMargin * 4), canvasImageWidth, canvasImageHeight);
+      }
+
+      pdf.save(`kDriveBi Activity Report.pdf`);
     });
-  }
-  changeTheme() {
-    this.isDarkMode=!this.isDarkMode;
-    if(this.isDarkMode){
-      this.currentTheme = 'dark-theme';
-    } else {
-      this.currentTheme = 'light-theme'
-    }
   }
 }
